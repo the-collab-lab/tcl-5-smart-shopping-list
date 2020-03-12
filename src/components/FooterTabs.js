6dd4a939644e5b7f3523e5c671a48face9bc54a5
Jon Bascos <jon@jonbascos.com>
2020-03-11 18:07:02 -0700

    created a pages folder containing shopping list and add item page.  Updated index.js with the router.

--- src/components/FooterTabs.js
+++ src/components/FooterTabs.js
@@ -1,41 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import ShoppingList from '../pages/ShoppingList';
+import AddItem from '../pages/AddItem';
 
 export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              {/* Link to be changed once pages are created*/}
-              <Link to="/">Shopping List</Link>
-            </li>
-            <li>
-              <Link to="/">Add Item</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-                renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/">
-            <ShoppingList />
-          </Route>
-          <Route path="/">
-            <AddItem />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function ShoppingList() {
-  return <h2>Shopping List</h2>;
-}
-
-function AddItem() {
-  return <h2>Add Item</h2>;
+	return (
+		<div>
+			<nav>
+				<ul>
+					<li>
+						{/* Link to be changed once pages are created*/}
+						<Link to='/ShoppingList'>Shopping List</Link>
+					</li>
+					<li>
+						<Link to='/AddItem'>Add Item</Link>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
 }
---
+++ src/pages/AddItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddItem = props => {
+	return <h1>Hello from Add Item</h1>;
+};
+
+export default AddItem;
