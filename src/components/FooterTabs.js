64e08e9a90b82b6a2cef6fb15759488a2fc956b8
diane1234 <dianekimlim@gmail.com>
2020-03-14 18:39:16 -0400

    Created components and pages

---
+++ src/components/FooterTabs.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import ShoppingList from '../pages/ShoppingList';
+import AddItem from '../pages/AddItem';
+import '../css/FooterTabs.css';
+
+export default function App(props) {
+	return (
+		<div>
+			<nav>
+				<ul>
+					<li>
+						{/* Link to be changed once pages are created*/}
+						<Link to='/ShoppingList'>
+							<button className='navbutton'>Shopping List</button>
+						</Link>
+					</li>
+					<li>
+						<Link to='/AddItem'>
+							<button className='navbutton'>Add Item</button>
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
+}
---
+++ src/pages/ShoppingList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ShoppingList = props => {
+	return <h1>Hello from the Shopping List</h1>;
+};
+
+export default ShoppingList;
