62d4b1522179ac50f7588e4ae54d352439e538bf
Jennifer Batara <jennifer.batara.dev@gmail.com>
2020-03-14 12:45:18 -0700

    Expand onClick function to include reading data from cloud FireStore

--- src/pages/FirebaseTest.js
+++ src/pages/FirebaseTest.js
@@ -5,7 +5,8 @@ export default function FirebaseTesting (props) {
 
   const [clickLog, setClickLog] = useState([]);
   const db = fb.firestore()
-
+  const clicksRef = db.collection("clicks");
+  
   async function onClick(event){
     event.preventDefault();
     const currentTime = Date.now();
@@ -16,11 +17,27 @@ export default function FirebaseTesting (props) {
     })
     .then(() => console.log(currentTime + " successfully written!"))
     .catch(error => console.error("Error writing document: ", error));
+
+    await clicksRef
+      .limit(3)
+      .get()
+      .then(function (querySnapshot) {
+        let clicksArray = [];
+        querySnapshot.forEach(function(doc){
+          let documentData = doc.data();
+          clicksArray.push(documentData);
+        });
+        setClickLog(clicksArray);
+    }).catch(function (error) {
+      console.log("Error getting document:", error);
+    });
   }
+
+
 
   return(
     <div>
       <a onClick={onClick} href='/FirebaseTesting'>Click me!</a>
     </div>
-  )
+  );
 }
