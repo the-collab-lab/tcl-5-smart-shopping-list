40fae0d39b9b39af3aa7eb06ae2275d26600ca5f
Jennifer Batara <jennifer.batara.dev@gmail.com>
2020-03-17 20:37:18 -0700

    Add NewListButton component to App home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import FirebaseTesting from './pages/FirebaseTest';
 import FooterTabs from './components/FooterTabs';
 import ShoppingList from './pages/ShoppingList';
 import AddItem from './pages/AddItem';
+import NewListButton from './components/newListButton'
 
 function App() {
+	const [token, setToken] = useState('')
+	
 	return (
 		<div className='App'>
 			<div>
       <a href="/FirebaseTesting">FirebaseTesting</a>
 				{/* A <Switch> looks through its children <Route>s and
                 renders the first one that matches the current URL. */}
+				<NewListButton />
 				<Switch>
 					<Route path='/ShoppingList'>
 						<ShoppingList />
