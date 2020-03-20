40a1ab4a90ba1d8bf632931ce65d5b45124ab312
Jon Bascos <jon@jonbascos.com>
2020-03-19 20:08:41 -0700

    Added console log to monitor local storeage token and state token.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
 import FirebaseTesting from './pages/FirebaseTest';
 import FooterTabs from './components/FooterTabs';
 import ShoppingList from './pages/ShoppingList';
 import AddItem from './pages/AddItem';
-import NewListButton from './components/newListButton'
+import NewListButton from './components/newListButton';
 import * as ls from 'local-storage';
 
 function App() {
-	const [token, setToken] = useState(ls.get("shoppingListToken"));
-	
+	const [token, setToken] = useState(ls.get('shoppingListToken'));
+	console.log('token', ls.get('shoppingListToken'), 'state', token);
+
 	return (
 		<div className='App'>
-			
-      <a href="/FirebaseTesting">FirebaseTesting</a>
-			{token?<Redirect to="/ShoppingList" />:<NewListButton setToken={setToken} token={token} />}
-				{/* A <Switch> looks through its children <Route>s and
+			<a href='/FirebaseTesting'>FirebaseTesting</a>
+			{token ? (
+				<Redirect to='/ShoppingList' />
+			) : (
+				<NewListButton setToken={setToken} token={token} />
+			)}
+			{/* A <Switch> looks through its children <Route>s and
                 renders the first one that matches the current URL. */}
-				<BrowserRouter>
+			<BrowserRouter>
 				<Switch>
 					<Route path='/ShoppingList'>
-						<ShoppingList token={token}/>
+						<ShoppingList token={token} />
 					</Route>
 					<Route path='/AddItem'>
 						<AddItem />
 					</Route>
-          <Route path='/FirebaseTesting'>
+					<Route path='/FirebaseTesting'>
 						<FirebaseTesting />
 					</Route>
 				</Switch>
-				</BrowserRouter>
+			</BrowserRouter>
 
 			<footer>
 				<FooterTabs />
