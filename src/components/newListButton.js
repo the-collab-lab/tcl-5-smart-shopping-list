2677fde78111615c2e7e68764a17a9ec3da4f1f0
Jon Bascos <jon@jonbascos.com>
2020-03-19 20:07:33 -0700

    Edited lcoal storage import.

--- src/components/newListButton.js
+++ src/components/newListButton.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import getToken from '../lib/tokenGenerator';
-import ls from "local-storage";
-var NewListButton = (props) => {
-
-    const onClick = (event) => {
-        const newToken = getToken();
-        ls.set("shoppingListToken", newToken);
-        props.setToken(newToken);
-    };
-    return(
-        <div>
-            <a href="/ShoppingList" onClick={onClick}>Create a New List</a>
-        </div>
-    );
+import * as ls from 'local-storage';
+var NewListButton = props => {
+	const onClick = event => {
+		const newToken = getToken();
+		ls.set('shoppingListToken', newToken);
+		props.setToken(newToken);
+	};
+	return (
+		<div>
+			<a href='/ShoppingList' onClick={onClick}>
+				Create a New List
+			</a>
+		</div>
+	);
 };
 
 export default NewListButton;
