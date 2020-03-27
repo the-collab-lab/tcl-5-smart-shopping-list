8dedae6662787fec390d3e11ef8a08f8666767fe
Jon Bascos <jon@jonbascos.com>
2020-03-26 21:33:11 -0700

    Removed unused imports due to all pages being in their own files now.  Removed const token and setToken since they were not being used anymore on App JS.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Link, Switch, Route, Redirect } from 'react-router-dom';
-import FirebaseTesting from './pages/FirebaseTest';
-import FooterTabs from './components/FooterTabs';
-import ShoppingList from './pages/ShoppingList';
-import AddItem from './pages/AddItem';
-import NewListButton from './components/NewListButton';
 import * as ls from 'local-storage';
 import Home from './pages/Home';
 
 function App() {
-	const [token, setToken] = useState(ls.get('shoppingListToken'));
-	console.log({ token });
-
 	return (
 		<div className='App'>
 			{/*Link </div>'/FirebaseTesting'>FirebaseTesting</Link>
@@ -36,7 +27,7 @@ function App() {
 			<footer>
 				<FooterTabs />
 			</footer>*/}
-			   <Home/>
+			<Home />
 		</div>
 	);
 }
