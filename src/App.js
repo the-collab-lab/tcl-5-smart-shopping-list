367003150daffffdc4c502bbbb74104f513e9d3a
Jon Bascos <jon@jonbascos.com>
2020-03-28 23:58:26 -0700

    Removed currently commented out Link to FirebaseTesting.  Removed commented out route to Home.  Removed commented out NewListButton import.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-  
 import React, { useState } from 'react';
 import './App.css';
-import {Switch, Route,} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import FirebaseTesting from './pages/FirebaseTest';
 import FooterTabs from './components/FooterTabs';
 import ShoppingList from './pages/ShoppingList';
 import AddItem from './pages/AddItem';
-/*import NewListButton from './components/NewListButton';*/
 import * as ls from 'local-storage';
 import Home from './pages/Home';
 
@@ -16,14 +14,6 @@ function App() {
 
 	return (
 		<div className='App'>
-			{/*<Link to='/FirebaseTesting'>FirebaseTesting</Link>
-			{token ? (
-				<Redirect to='/ShoppingList' />
-			) : (
-				<NewListButton setToken={setToken} token={token} />
-			)}
-			 A <Switch> looks through its children <Route>s and
-                renders the first one that matches the current URL. */}
 			<Switch>
 				<Route path='/ShoppingList'>
 					<ShoppingList token={token} />
@@ -34,11 +24,8 @@ function App() {
 				<Route path='/FirebaseTesting'>
 					<FirebaseTesting />
 				</Route>
-				{/*<Route path='/Home'>
-					<Home token={token} />
-				</Route>*/}
 			</Switch>
-                 <Home token={token} setToken={setToken}/>
+			<Home token={token} setToken={setToken} />
 			<footer>
 				<FooterTabs />
 			</footer>
