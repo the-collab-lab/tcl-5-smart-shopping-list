ba8d870cb8d92f134c83456a774233710da1485d
Jon Bascos <jon@jonbascos.com>
2020-04-01 19:01:37 -0700

    created input checkbox for every item clicked on.

--- src/pages/ShoppingList.js
+++ src/pages/ShoppingList.js
@@ -26,15 +26,23 @@ const ShoppingList = ({ token }) => {
 			setShoppingListItems(allData);
 		});
 	}, []);
-	console.log('items: ', shoppingListItems);
-	// console.log(shoppingListItems[0].itemName);
+
+	const handleCheck = e => {
+		alert(e.target.value);
+	};
 
 	return (
-		// figure out how to check only 1 item and not all list
 		<div>
 			<ul>
 				{shoppingListItems.map(item => (
-					<li key={item.id}>{item.itemName}</li>
+					<div>
+						<input
+							type='checkbox'
+							value={item.itemName}
+							onChange={handleCheck}
+						/>{' '}
+						{item.itemName}
+					</div>
 				))}
 			</ul>
 		</div>
@@ -42,77 +50,3 @@ const ShoppingList = ({ token }) => {
 };
 
 export default ShoppingList;
-
-// import React, { useState, useEffect } from 'react';
-// import fb from '../lib/firebase';
-
-// const ShoppingList = ({ token }) => {
-// 	const [shoppingListItems, setShoppingListItems] = useState([]);
-// 	const [isChecked, setIsChecked] = useState(false);
-// 	const [itemsChecked, setItemsChecked] = useState([]);
-
-// 	//Thanks for sharing your code!!
-// 	useEffect(() => {
-// 		const db = fb.firestore();
-// 		const tokenRef = db.collection(token);
-
-// 		tokenRef
-// 			.orderBy('timeFrame', 'asc')
-// 			.get()
-// 			.then(querySnapshot => {
-// 				let fullCollection = [];
-// 				querySnapshot.forEach(doc => {
-// 					let documentData = doc.data();
-// 					let nameData = documentData.itemName;
-// 					// let itemId = documentData.id; // just needs Ids of checked items
-// 					nameData = nameData
-// 						.toLowerCase()
-// 						.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
-// 						.trim()
-// 						.replace(/\s{2,}/g, ' ');
-// 					fullCollection.push(nameData);
-// 				});
-// 				setShoppingListItems(fullCollection);
-// 				// setCheckedId(itemId) // push all checked Ids to an array
-// 			})
-// 			.catch(error => {
-// 				console.log('Error getting document:', error);
-// 			});
-// 	}, []);
-
-// 	console.log('items: ', shoppingListItems);
-
-// 	const handleInputChange = (e, id) => {
-// 		// need to change handleSubmit for more specific to item
-// 		// setIsChecked(isChecked ? false : true);
-// 		// console.log(e.target.value);
-// 		// console.log(e.target.id);
-// 		// if (e.target.value) {
-// 		// 	setIsChecked(isChecked ? false : true);
-// 		// }
-// 	};
-
-// 	return (
-// 		// figure out how to check only 1 item and not all list
-// 		<form>
-// 			{shoppingListItems.length > 0 &&
-// 				shoppingListItems.map(item => (
-// 					<div>
-// 						<label>
-// 							{item}
-// 							<input
-// 								id={item.id}
-// 								name={item}
-// 								value={item}
-// 								type='checkbox'
-// 								checked={isChecked}
-// 								onClick={handleInputChange}
-// 							/>
-// 						</label>
-// 					</div>
-// 				))}
-// 		</form>
-// 	);
-// };
-
-// export default ShoppingList;
