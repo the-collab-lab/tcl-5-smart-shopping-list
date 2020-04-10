b6c2d635435c6868dcdaab9cefaa81b634b9b63c
Ashling <hennessyaisling9@gmail.com>
2020-04-09 17:50:28 -0700

    Revert "Revert "Merge branch 'AH-DK-estimate-record-purchase-date' of https://github.com/the-collab-lab/tcl-5-smart-shopping-list into AH-DK-estimate-record-purchase-date""
    This reverts commit d503c96bb40de1de3b7bfde7a00324c64a38af0b.

--- src/pages/ShoppingList.js
+++ src/pages/ShoppingList.js
@@ -3,16 +3,23 @@ import { useHistory } from 'react-router-dom';
 import fb from '../lib/firebase';
 import moment from 'moment';
 import calculateEstimate from '../lib/estimates';
+<<<<<<< HEAD
 
 const HOURS24 = 86400; //24 hours in seconds
 
+=======
+const HOURS24 = 86400; //24 hours in seconds
+>>>>>>> 4d4f3da416d2347224c6a10196dfec819026b2a8
 const ShoppingList = ({ token }) => {
     const [shoppingListItems, setShoppingListItems] = useState([]);
     const userToken = token;
     let history = useHistory();
+<<<<<<< HEAD
   
   
     
+=======
+>>>>>>> 4d4f3da416d2347224c6a10196dfec819026b2a8
     /*const handleChange = (e, item) => {
         if (item.datePurchased) {
           let lastEstimate;
@@ -49,6 +56,7 @@ const ShoppingList = ({ token }) => {
             .update({ datePurchased, numOfPurchases: item.numOfPurchases + 1 }):
         }    
     };*/
+<<<<<<< HEAD
 
 
 	const welcomeInstructions = () => {
@@ -81,6 +89,35 @@ const ShoppingList = ({ token }) => {
 	};
 
 
+=======
+  const welcomeInstructions = () => {
+    return (
+      <div>
+        <input
+            type="checkbox"
+            className="button-link"
+            id="WelcomeClick"
+          />
+          <label htmlFor="WelcomeClick" id="Welcome">
+          Your list looks empty. Need help?
+          </label>
+          <div id="hideWelcome">
+            <ul>
+              <li>
+              Add items by clicking the "Add Item" button in the bottom of the screen.
+              </li>
+              <li>
+              Your list will be sorted with most needed items first.
+              </li>
+              <li>
+              To share this list with you friend, give them the code "{token}"
+              </li>
+            </ul>
+          </div>  
+        </div>
+    );
+  };
+>>>>>>> 4d4f3da416d2347224c6a10196dfec819026b2a8
     const getShoppingList = () => {
         const db = fb.firestore();
         if (userToken) {
@@ -105,24 +142,32 @@ const ShoppingList = ({ token }) => {
             history.push('/Home');
         }
     };
-
     useEffect(() => {
         getShoppingList();
     }, []);
+<<<<<<< HEAD
 
 
     console.log("SHOPPING LIST ITEMS", shoppingListItems);
 
 
+=======
+    console.log("SHOPPING LIST ITEMS", shoppingListItems);
+>>>>>>> 4d4f3da416d2347224c6a10196dfec819026b2a8
     const lessThan24Hours = date => {
         const formattedDate = parseInt(moment(date).format());
         const newDate = moment(Date.now());
         const lastPurchase = moment(formattedDate);
         return lastPurchase.diff(newDate, 'hours') < 24;
     };
+<<<<<<< HEAD
 
     const handleCheck = e => {
 
+=======
+    const handleCheck = (e,item) => {
+        console.log("ITEMMMMMM", item)
+>>>>>>> 4d4f3da416d2347224c6a10196dfec819026b2a8
         if (item.lastPurchaseDate) {
             let lastEstimate;
             item.nextPurchaseDate
@@ -150,6 +195,7 @@ const ShoppingList = ({ token }) => {
                 lastEstimate,
                 nextPurchaseDate,
                 isChecked: e.target.checked
+<<<<<<< HEAD
               });
           } else {
             let lastPurchaseDate = moment(Date.now()).format();
@@ -162,6 +208,27 @@ const ShoppingList = ({ token }) => {
 
 
 
+=======
+              })
+              .then(function() {
+                getShoppingList();
+            });
+          } else {
+            let lastPurchaseDate = moment(Date.now()).format();
+            let db = fb.firestore();
+            db.collection(userToken)
+              .doc(e.target.name)
+              .update({
+                  isChecked: e.target.checked,
+                  lastPurchaseDate, 
+                  numOfPurchases:item.numOfPurchases + 1 
+                })
+              .then(function() {
+                    getShoppingList();
+              });
+        }
+    };
+>>>>>>> 4d4f3da416d2347224c6a10196dfec819026b2a8
         /*console.log("DIANE HEREEEEEEE  TIMEFRAME =============", e.target.value)
         console.log("DIANE HEREEEEEEE PURCHASE DATE =============", e.target.name)
         let db = fb.firestore();
@@ -176,10 +243,11 @@ const ShoppingList = ({ token }) => {
         tokenRef.update(dataCheck).then(function() {
             getShoppingList();
         });
-    };
+    };*/
     return (
         <div>
             <ul>
+<<<<<<< HEAD
 				{shoppingListItems.length>0?
 					(shoppingListItems.map(item => (
 						<div>
@@ -197,9 +265,27 @@ const ShoppingList = ({ token }) => {
 							/>{' '}
 							{item.itemName}
 						</div>
+=======
+        {shoppingListItems.length>0?
+          (shoppingListItems.map(item => (
+            <div>
+              <input
+                key={item.id}
+                id={item.id}
+                type="checkbox"
+                name={item.id}
+                                value={item.isChecked}
+                                //name={item.lastPurchaseDate} to pull LAst Purchase date data
+                                //value={item.timeFrame} to pull timeFrame data
+                checked={item.isChecked}
+                onChange={e => handleCheck(e, item)}
+              />{' '}
+              {item.itemName}
+            </div>
+>>>>>>> 4d4f3da416d2347224c6a10196dfec819026b2a8
                 ))):(
-					welcomeInstructions()
-				)}
+          welcomeInstructions()
+        )}
             </ul>
         </div>
     
