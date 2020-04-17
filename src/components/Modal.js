6fac692f551a49184e0a30383a909e4854ddaba9
Jon Bascos <jon@jonbascos.com>
2020-04-16 18:57:27 -0700

    Cleaned up committed lines.  Removed "item" from Modal.  Fixed onClick for Yes button and No button.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../App.css';
-import fb from '../lib/firebase';
 
-//const [setModal] = useState(false);
-//const [deleteItem, deleteItemMessage] = useState();
-
-const Modal = (item, props) => {
+const Modal = props => {
     return (
         <div className="Modal">
-            <p> Are you sure you want to delete this item?</p>
-            <button onClick={() => props.deleteItem(props.item.id)}>YES</button>
-            <button>No</button>
-            {/*we want ot return to the shopping list we were on, however so need to fix this*/}
+            <p> Are you sure you want to delete {props.item.itemName}?</p>
+            <button onClick={() => props.delete(props.item)}>YES</button>
+            <button onClick={() => props.cancel()}>No</button>
         </div>
     );
 };
