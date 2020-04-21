5a8dbfb6b5ec5b6a25434338ba279bb4cf2dea6c
diane1234 <dianekimlim@gmail.com>
2020-04-21 15:39:50 -0400

    Only committing estimates.js

--- src/lib/estimates.js
+++ src/lib/estimates.js
@@ -5,16 +5,19 @@
  * @param {Number} latestInterval The interval between the most recent and previous purchases
  * @param {Number} numberOfPurchases Total number of purchases for the item
  */
-const calculateEstimate = (lastEstimate, latestInterval, numberOfPurchases) => {
-    if (isNaN(lastEstimate)) {
-        lastEstimate = 14;
-    }
 
-    // FIXME algorithm doesn't work when there's only 1 purchase in the database
-    let previousFactor = lastEstimate * numberOfPurchases;
-    let latestFactor = latestInterval * (numberOfPurchases - 1);
-    let totalDivisor = numberOfPurchases * 2 - 1;
-    return (previousFactor + latestFactor) / totalDivisor;
+const calculateEstimate = (lastEstimate, latestInterval, numberOfPurchases) => {
+    if (numberOfPurchases > 1) {
+        if (isNaN(lastEstimate)) {
+                lastEstimate = 14;
+        }
+        let previousFactor = lastEstimate * numberOfPurchases;
+        let latestFactor = latestInterval * (numberOfPurchases - 1);
+        let totalDivisor = numberOfPurchases * 2 - 1;
+        return Math.round((previousFactor + latestFactor) / totalDivisor);
+    } else {
+        return latestInterval;
+    }
 };
 
 export default calculateEstimate;
