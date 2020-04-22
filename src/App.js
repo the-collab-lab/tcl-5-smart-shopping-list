3f13e07f15b87d0f5614291e0e944c011273bf75
Jon Bascos <jon@jonbascos.com>
2020-04-21 22:13:19 -0700

    Moved Home page into the Switch to prevent Home from always being displayed.

--- src/App.js
+++ src/App.js
@@ -9,28 +9,29 @@ import * as ls from 'local-storage';
 import Home from './pages/Home';
 
 function App() {
-	const [token, setToken] = useState(ls.get('shoppingListToken'));
+    const [token, setToken] = useState(ls.get('shoppingListToken'));
 
-
-	return (
-		<div className='App'>
-			<Switch>
-				<Route path='/ShoppingList'>
-					<ShoppingList token={token} />
-				</Route>
-				<Route path='/AddItem'>
-					<AddItem token={token} />
-				</Route>
-				<Route path='/FirebaseTesting'>
-					<FirebaseTesting />
-				</Route>
-			</Switch>
-			<Home token={token} setToken={setToken} />
-			<footer>
-				<FooterTabs />
-			</footer>
-		</div>
-	);
+    return (
+        <div className="App">
+            <Switch>
+                <Route exact path="/">
+                    <Home token={token} setToken={setToken} />
+                </Route>
+                <Route path="/ShoppingList">
+                    <ShoppingList token={token} />
+                </Route>
+                <Route path="/AddItem">
+                    <AddItem token={token} />
+                </Route>
+                <Route path="/FirebaseTesting">
+                    <FirebaseTesting />
+                </Route>
+            </Switch>
+            <footer>
+                <FooterTabs />
+            </footer>
+        </div>
+    );
 }
 
 export default App;
