f026a0ba3ba5bd85f29ce8fa6eb45c46fa957879
Jon Bascos <jon@jonbascos.com>
2020-04-22 00:46:24 -0700

    Imported JoinList page.  Created JoinList Route and added handleChange, checkToken and inputToken props to JoinList.  Moved handleChange and checkToken from Home and into App so JoinList could pull data from App. Also imported Firebase.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,37 @@ import FirebaseTesting from './pages/FirebaseTest';
 import FooterTabs from './components/FooterTabs';
 import ShoppingList from './pages/ShoppingList';
 import AddItem from './pages/AddItem';
+import fb from './lib/firebase';
 import * as ls from 'local-storage';
 import Home from './pages/Home';
+import JoinList from './pages/JoinList';
 
 function App() {
     const [token, setToken] = useState(ls.get('shoppingListToken'));
+    const [inputToken, setInputToken] = useState('');
 
+    const handleChange = e => {
+        setInputToken(e.target.value);
+    };
+
+    const checkToken = e => {
+        e.preventDefault();
+        let db = fb.firestore();
+        if (inputToken.length === 0) {
+            alert('You must enter a token value.  Try again! ');
+        } else {
+            db.collection(`${inputToken}`)
+                .get()
+                .then(data => {
+                    if (data.empty === true) {
+                        alert('Bad token, Try again or Create a New List');
+                    } else {
+                        ls.set('ShoppingListToken', inputToken);
+                        setToken(inputToken);
+                    }
+                });
+        }
+    };
     return (
         <div className="App">
             <Switch>
@@ -25,6 +50,13 @@ function App() {
                 </Route>
                 <Route path="/FirebaseTesting">
                     <FirebaseTesting />
+                </Route>
+                <Route path="/JoinList">
+                    <JoinList
+                        handleChange={handleChange}
+                        checkToken={checkToken}
+                        inputToken={inputToken}
+                    />
                 </Route>
             </Switch>
             <footer>
