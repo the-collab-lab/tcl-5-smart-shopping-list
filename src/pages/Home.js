98c71a6b6edd9286977a1cd6a02aa6caa6424842
diane1234 <dianekimlim@gmail.com>
2020-04-23 00:38:37 -0400

    Modified css of homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import { Link, Redirect } from 'react-router-dom';
 function Home({ token, setToken }) {
     return (
         <div className="Home">
-            <h1>Welcome to your smart shopping list!</h1>
+            <h1 className="Home-h1">Welcome to your smart shopping list!</h1>
             <h2>Tap "Create shopping list" to get started</h2>
             <NewListButton
                 className="button"
@@ -14,7 +14,7 @@ function Home({ token, setToken }) {
                 token={token}
             />
             <p>
-                <h2>
+                <h2 className="Home-h2">
                     You can even{' '}
                     <Link to="/JoinList">join an existing list.</Link>
                 </h2>
