e28f7862a4fa1d6267c9c644db4f3d8bead667a1
Jon Bascos <jon@jonbascos.com>
2020-04-23 17:44:52 -0700

    created joinList-Container and JoinList-text classes to style their respective items.

--- src/pages/JoinList.js
+++ src/pages/JoinList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const JoinList = ({handleChange, checkToken, inputToken}) => {
+const JoinList = ({ handleChange, checkToken, inputToken }) => {
     return (
-        <div>
+        <div className="joinList-container">
+            <div className="JoinList-text">
+                Enter the token of an existing shopping list.
+            </div>
             <input
+                className="joinList-input"
                 type="text"
                 name="inputToken"
                 placeholder="Enter Token"
