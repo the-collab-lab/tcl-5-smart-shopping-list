58f0ca17b082c0f6181b52fe5bd523e415109638
diane1234 <dianekimlim@gmail.com>
2020-05-03 12:43:28 -0400

    Update buttons of footertabs

--- src/components/FooterTabs.js
+++ src/components/FooterTabs.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {Button} from 'react-materialize'
 
 const FooterTabs= () => {
 	return (
 			<div class='page-footer white'>
 				<div className='container center'>
 						<Link to='/ShoppingList'>
-						<button className='navbutton'>Shopping List</button>
+						<Button
+						node="button"
+						waves="light"
+						className='navbutton btn-large'
+						>
+						Shopping List
+						</Button>
+						{/* <button className='navbutton'>Shopping List</button> */}
 						</Link>
 						<Link to='/AddItem'>
-						<button className='navbutton'>Add Item</button>
+						<Button
+						node="button"
+						waves="light"
+						className='navbutton btn-large'
+						>
+						Add Item
+						</Button>
+						{/* <button className='navbutton'>Add Item</button> */}
 						</Link>
 				</div>
 			</div>
